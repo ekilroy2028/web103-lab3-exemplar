7e31aea0a9628cf81986b575d07deb19bb917cbf
ekilroy2028 <ekilroy1@asu.edu>
2026-06-24 19:29:32 -0400

    Add create, update, delete routes

--- server_backend/routes/gifts.js
+++ server_backend/routes/gifts.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getGifts } from '../controllers/gifts.js'
+import { getGifts, getGiftById, createGift, updateGift, deleteGift } from '../controllers/gifts.js'
 
 const router = express.Router()
 
 // GET /gifts/
 router.get('/', getGifts)
+router.get('/:giftId', getGiftById)
+router.post('/', createGift)
+router.delete('/:id', deleteGift)
+router.patch('/:id', updateGift)
 
 export default router
